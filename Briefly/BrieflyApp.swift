f3f6d3ad0f3ebfc34208fdf1f7bc72c48262524b
pranay chander <chander.pranay@gmail.com>
2025-12-14 20:50:51 +0530

    chore: add initial Xcode project files and app skeleton

--- Briefly/BrieflyApp.swift
+++ Briefly/BrieflyApp.swift
@@ -10,12 +10,9 @@ import CoreData
 
 @main
 struct BrieflyApp: App {
-    let persistenceController = PersistenceController.shared
-
     var body: some Scene {
         WindowGroup {
             ContentView()
-                .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
     }
 }
